e6bccc602777318db4f56b3bc9e4a4074862db4b
BK-TN <me@bk-tn.com>
2016-04-04 20:51:23 +0200

    Rewrote the server list to work with UNet matchmaking

---
+++ Assets/Scripts/UI/MatchListItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+namespace Sanicball.UI
+{
+    public class MatchListItem : MonoBehaviour
+    {
+        public Text serverNameText;
+        public Text serverStatusText;
+        public Text playerCountText;
+        public Text pingText;
+        public Image pingLoadingImage;
+
+        //TODO: Implement pinging matches
+        //private bool pingDone = false;
+        //private float pingTimeout = 8f;
+
+        public void SetData(MatchDesc data)
+        {
+            //info = new ServerInfo(data);
+            serverNameText.text = data.name;
+            serverStatusText.text = "(STATUS GOES HERE)";
+            playerCountText.text = data.currentSize + "/" + data.maxSize;
+        }
+    }
+}
--- Assets/Scripts/UI/OnlinePanel.cs
+++ Assets/Scripts/UI/OnlinePanel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking.Match;
 using UnityEngine.UI;
 
 namespace Sanicball.UI
@@ -10,44 +11,18 @@ namespace Sanicball.UI
         public Image spinnerImage;
         public Text errorField;
         public Text serverCountField;
-        public ServerListItem serverListItemPrefab;
+        public MatchListItem matchButtonPrefab;
         public Selectable aboveList;
         public Selectable belowList;
         private string masterServerGameName = "sanicball";
         private bool refreshing = false;
 
-        private List<ServerListItem> servers = new List<ServerListItem>();
-
-        public void RefreshServers()
-        {
-            if (refreshing) return;
-
-            Debug.Log("Connecting to master server...");
-
-            serverCountField.text = "Refreshing...";
-            spinnerImage.enabled = true;
-            refreshing = true;
-            errorField.enabled = false;
-            //Clear old servers
-            foreach (var serv in servers)
-            {
-                Destroy(serv.gameObject);
-            }
-            servers.Clear();
-            MasterServer.RequestHostList(masterServerGameName);
-        }
-
-        public void OnFailedToConnectToMasterServer(NetworkConnectionError error)
-        {
-            spinnerImage.enabled = false;
-            refreshing = false;
-            errorField.enabled = true;
-            errorField.text = "Could not get server list! Try again later.";
-            serverCountField.text = servers.Count + (servers.Count == 1 ? " server" : " servers");
-        }
+        private NetworkMatch matchFinder;
+        private List<MatchListItem> latestMatches = new List<MatchListItem>();
 
         private void Awake()
         {
+            matchFinder = gameObject.AddComponent<NetworkMatch>();
             masterServerGameName = "sanicball" + GameVersion.AsFloat;
             errorField.enabled = false;
         }
@@ -57,67 +32,96 @@ namespace Sanicball.UI
             //Refresh on f5 (pretty nifty)
             if (Input.GetKeyDown(KeyCode.F5))
             {
-                RefreshServers();
+                RefreshMatches();
             }
         }
 
-        private void OnMasterServerEvent(MasterServerEvent e)
+        public void RefreshMatches()
         {
-            if (e == MasterServerEvent.HostListReceived)
-            {
-                spinnerImage.enabled = false;
-                refreshing = false;
-                //Create new servers
-                var hostList = MasterServer.PollHostList();
-                Debug.Log(hostList.Length + " servers recieved.");
-                serverCountField.text = hostList.Length + (hostList.Length == 1 ? " server" : " servers");
-                if (hostList.Length == 0)
-                {
-                    errorField.enabled = true;
-                    errorField.text = @"No servers ¯\_(ツ)_/¯";
-                }
-                errorField.enabled = hostList.Length == 0;
-                foreach (var host in hostList)
-                {
-                    var server = Instantiate(serverListItemPrefab);
-                    server.transform.SetParent(targetServerListContainer, false);
-                    server.SetData(host);
-                    servers.Add(server);
-                }
-                //Add navigation links
-                for (var i = 0; i < servers.Count; i++)
-                {
-                    var button = servers[i].GetComponent<Button>();
-                    if (button)
-                    {
-                        var nav = new Navigation() { mode = Navigation.Mode.Explicit };
-                        //Up navigation
-                        if (i == 0)
-                        {
-                            nav.selectOnUp = aboveList;
-                            var nav2 = aboveList.navigation;
-                            nav2.selectOnDown = button;
-                            aboveList.navigation = nav2;
-                        }
-                        else
-                        {
-                            nav.selectOnUp = servers[i - 1].GetComponent<Button>();
-                        }
-                        //Down navigation
-                        if (i == servers.Count - 1)
-                        {
-                            nav.selectOnDown = belowList;
-                            var nav2 = belowList.navigation;
-                            nav2.selectOnUp = button;
-                            belowList.navigation = nav2;
-                        }
-                        else
-                        {
-                            nav.selectOnDown = servers[i + 1].GetComponent<Button>();
-                        }
+            if (refreshing) return;
 
-                        button.navigation = nav;
+            Debug.Log("Listing matches...");
+
+            serverCountField.text = "Refreshing...";
+            spinnerImage.enabled = true;
+            refreshing = true;
+            errorField.enabled = false;
+            //Clear old matches
+            foreach (var serv in latestMatches)
+            {
+                Destroy(serv.gameObject);
+            }
+            latestMatches.Clear();
+
+            matchFinder.ListMatches(0, 10, "", ListMatchesCallback);
+        }
+
+        private void ListMatchesCallback(ListMatchResponse response)
+        {
+            if (!response.success)
+            {
+                errorField.enabled = true;
+                errorField.text = "Could not connect to the matchmaking service\n(" + response.extendedInfo + ")";
+                return;
+            }
+
+            Debug.Log(response.matches.Count + " matches found");
+            refreshing = false;
+            spinnerImage.enabled = false;
+
+            serverCountField.text = response.matches.Count + " active matches";
+            if (response.matches.Count > 0)
+            {
+                errorField.enabled = false;
+            }
+            else
+            {
+                errorField.enabled = true;
+                errorField.text = @"No matches to join ¯\_(ツ)_/¯";
+            }
+
+            //Create match list items for all matches found
+            foreach (var match in response.matches)
+            {
+                var matchListItem = Instantiate(matchButtonPrefab);
+                matchListItem.transform.SetParent(targetServerListContainer, false);
+                matchListItem.SetData(match);
+                latestMatches.Add(matchListItem);
+            }
+
+            //Add navigation links
+            for (var i = 0; i < latestMatches.Count; i++)
+            {
+                var button = latestMatches[i].GetComponent<Button>();
+                if (button)
+                {
+                    var nav = new Navigation() { mode = Navigation.Mode.Explicit };
+                    //Up navigation
+                    if (i == 0)
+                    {
+                        nav.selectOnUp = aboveList;
+                        var nav2 = aboveList.navigation;
+                        nav2.selectOnDown = button;
+                        aboveList.navigation = nav2;
                     }
+                    else
+                    {
+                        nav.selectOnUp = latestMatches[i - 1].GetComponent<Button>();
+                    }
+                    //Down navigation
+                    if (i == latestMatches.Count - 1)
+                    {
+                        nav.selectOnDown = belowList;
+                        var nav2 = belowList.navigation;
+                        nav2.selectOnUp = button;
+                        belowList.navigation = nav2;
+                    }
+                    else
+                    {
+                        nav.selectOnDown = latestMatches[i + 1].GetComponent<Button>();
+                    }
+
+                    button.navigation = nav;
                 }
             }
         }
